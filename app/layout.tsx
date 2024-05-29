702fbfcd7a83bcaea5f8065119454883de8bcae3
wesselpawel <wesselpawel@wp.pl>
2024-05-30 01:17:51 +0200

    recriutment form

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,32 @@ export const metadata: Metadata = {
     "dotacje, finansowanie, termomodernizacja, remont, modernizacja energetyczna, oszczędność energii, efektywność energetyczna, wsparcie finansowe, programy rządowe, fundusze unijne, poprawa izolacji, renowacja budynków, energooszczędność, zrównoważony rozwój, dotacje unijne",
   icons: [
     {
-      url: "/favicon.ico",
+      url: "/favicons/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    {
+      url: "/favicons/android-chrome-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      url: "/favicons/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+    {
+      url: "/favicons/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      url: "/favicons/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+    {
+      url: "/favicons/favicon.ico",
       sizes: "48x48",
       type: "image/x-icon",
     },
