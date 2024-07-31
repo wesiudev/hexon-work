e81d2c7a4bfdc8d876c7639719a5893b98131280
wesselpawel <wesselpawel@wp.pl>
2024-07-31 02:25:36 +0200

    wysiwyg editor

--- app/admin/leads/leads/page.tsx
+++ app/admin/leads/leads/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { getLeads } from "@/common/firebase";
-import Link from "next/link";
 import Leads from "./Leads";
 
-export default async function Page() {
+export default function Page() {
   return <Leads />;
 }
